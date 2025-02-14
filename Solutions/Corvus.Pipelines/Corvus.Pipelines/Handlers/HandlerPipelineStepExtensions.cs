794da5de82e03bd42b02a33ea57cd39f4b48c23f
Ian Griffiths <Ian.Griffiths@endjin.com>
2025-02-14 11:57:13 +0000

    Add Evaluate extension method for bool-producing handler pipelines

---
+++ Solutions/Corvus.Pipelines/Corvus.Pipelines/Handlers/HandlerPipelineStepExtensions.cs
@@ -0,0 +1,29 @@
+﻿// <copyright file="HandlerPipelineStepExtensions.cs" company="Endjin Limited">
+// Copyright (c) Endjin Limited. All rights reserved.
+// </copyright>
+
+namespace Corvus.Pipelines.Handlers;
+
+/// <summary>
+/// Extension methods for working with handler pipeline steps.
+/// </summary>
+public static class HandlerPipelineStepExtensions
+{
+    /// <summary>
+    /// Evaluates a handler pipeline whose result type is <see langword="bool" />.
+    /// </summary>
+    /// <typeparam name="TInput">Input type.</typeparam>
+    /// <param name="handler">The handler to evaluate.</param>
+    /// <param name="input">The input to pass to the handler.</param>
+    /// <returns>
+    /// <see langword="true"/> if the handler handled the input and returned <see langword="true"/>;
+    /// <see langword="false"/> otherwise.
+    /// </returns>
+    public static bool Evaluate<TInput>(
+        this SyncPipelineStep<HandlerState<TInput, bool>> handler,
+        TInput input)
+    {
+        return handler(HandlerState<TInput, bool>.For(input))
+            .WasHandled(out bool matched) && matched;
+    }
+}
