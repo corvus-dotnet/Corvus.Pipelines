e876229c55fb26a23e70efa3c4c7718e0633aed3
Ian Griffiths <Ian.Griffiths@endjin.com>
2025-06-18 12:32:00 +0100

    Add WWW-Authenticate support in NonForwardedResponseDetails

--- Solutions/Corvus.YarpPipelines/Corvus.YarpPipelines/NonForwardedResponseDetails.cs
+++ Solutions/Corvus.YarpPipelines/Corvus.YarpPipelines/NonForwardedResponseDetails.cs
@@ -15,10 +15,21 @@ public readonly struct NonForwardedResponseDetails
     private readonly string? redirectLocation;
     private readonly ImmutableArray<CookieDetails>? cookieDetails;
     private readonly bool redirectShouldPreserveMethod;
+    private readonly string? wwwAuthenticateHeaderValue;
 
     private NonForwardedResponseDetails(int statusCode)
     {
         this.StatusCode = statusCode;
+    }
+
+    private NonForwardedResponseDetails(int statusCode, string wwwAuthenticateHeaderValue)
+    {
+        this.StatusCode = statusCode;
+
+        // For now, we're handling the WWW-Authenticate header as a special case, because that and cookies are
+        // the only scenarios in which we need to add or modify response headers. If more scenarios arise, we
+        // will most likely generalise header handling.
+        this.wwwAuthenticateHeaderValue = wwwAuthenticateHeaderValue;
     }
 
     private NonForwardedResponseDetails(
@@ -117,6 +128,17 @@ public readonly struct NonForwardedResponseDetails
     }
 
     /// <summary>
+    /// Creates a <see cref="NonForwardedResponseDetails"/> indicating that an Unauthorized (401) response should be
+    /// produced, along with a WWW-Authenticate header value.
+    /// </summary>
+    /// <param name="wwwAuthenticateHeaderValue">The value for the WWW-Authenticate header.</param>
+    /// <returns>A <see cref="NonForwardedResponseDetails"/>.</returns>
+    public static NonForwardedResponseDetails ForUnauthorized(string wwwAuthenticateHeaderValue)
+    {
+        return new NonForwardedResponseDetails(401, wwwAuthenticateHeaderValue);
+    }
+
+    /// <summary>
     /// Gets the redirect location if there is one.
     /// </summary>
     /// <param name="result">
@@ -139,7 +161,47 @@ public readonly struct NonForwardedResponseDetails
     }
 
     /// <summary>
-    /// Details returned  from <see cref="TryGetRedirect(out RedirectDetails)"/>.
+    /// Gets the number of headers that should be added to the response.
+    /// </summary>
+    /// <returns>The number of headers.</returns>
+    public int GetHeadersToAddCount()
+    {
+        return this.wwwAuthenticateHeaderValue is not null ? 1 : 0;
+    }
+
+    /// <summary>
+    /// Reads the headers that should be added to the response into a span, if there is space to do so.
+    /// </summary>
+    /// <param name="headersToAdd">
+    /// The span into which to write the headers. If this is too small to hold all of the headers, then
+    /// no headers will be written to it, and the method will return false.
+    /// </param>
+    /// <param name="headerCount">
+    /// The number of headers. This will be set to the total number of headers even if the span was too small.
+    /// </param>
+    /// <returns>
+    /// True if the span was large enough to contain the headers, and was therefore populated. False if it was not.
+    /// </returns>
+    public bool TryGetHeadersToAdd(Span<HeaderDetails> headersToAdd, out int headerCount)
+    {
+        if (this.wwwAuthenticateHeaderValue is not null)
+        {
+            headerCount = 1;
+            if (headersToAdd.Length < 1)
+            {
+                return false;
+            }
+
+            headersToAdd[0] = new HeaderDetails("WWW-Authenticate", this.wwwAuthenticateHeaderValue);
+            return true;
+        }
+
+        headerCount = 0;
+        return false;
+    }
+
+    /// <summary>
+    /// Details returned from <see cref="TryGetRedirect(out RedirectDetails)"/>.
     /// </summary>
     /// <param name="Location">
     /// The redirection location, in a fully escaped form suitable for use in HTTP headers and
@@ -156,4 +218,11 @@ public readonly struct NonForwardedResponseDetails
     /// A list of changes to make to the cookies in the response, or null if no changes are required.
     /// </param>
     public readonly record struct RedirectDetails(string Location, bool Permanent, bool PreserveMethod, ImmutableArray<CookieDetails>? CookieDetails);
+
+    /// <summary>
+    /// A header returned from <see cref="TryGetHeadersToAdd(Span{HeaderDetails}, out int)"/>.
+    /// </summary>
+    /// <param name="Name">The header name.</param>
+    /// <param name="Value">The header value.</param>
+    public readonly record struct HeaderDetails(string Name, string Value);
 }
